272252c31f90f85062d4d51b15df3605a35ac647
Flavia Mabras <flaviamabras@hotmail.com>
2023-01-08 21:34:49 +0100

    exercises 1.5 solved

--- Part 1/courseinfo/src/components/Total.js
+++ Part 1/courseinfo/src/components/Total.js
@@ -1,3 +1,7 @@
-const Total = (props) => <p>Number of exercises {props.total}</p>;
+const Total = ({ parts }) => {
+  let total = 0;
+  parts.forEach((part) => (total += part.exercises));
+  return <p>Number of exercises {total}</p>;
+};
 
 export default Total;
