5eacce76768ef5bd717c0a7507f53b6f73c9605c
Flavia Mabras <flaviamabras@hotmail.com>
2023-01-22 20:56:28 +0100

    exercise 2.2 solved

--- Part 2/courseinfo/src/components/Total.js
+++ Part 2/courseinfo/src/components/Total.js
@@ -1,7 +1,11 @@
 const Total = ({ parts }) => {
   let total = 0;
   parts.forEach((part) => (total += part.exercises));
-  return <p>Number of exercises {total}</p>;
+  return (
+    <p>
+      <strong>Total of {total} exercises</strong>
+    </p>
+  );
 };
 
 export default Total;
