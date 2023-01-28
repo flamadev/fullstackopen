5bb6af719ca10791696035407ec64b86f2816add
Flavia Mabras <flaviamabras@hotmail.com>
2023-01-28 19:33:58 +0100

    exercise 2.9 solved

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-1234567' },
+    { name: 'Arto Hellas', phone: '040-123456' },
+    { name: 'Ada Lovelace', phone: '39-44-5323523' },
+    { name: 'Dan Abramov', phone: '12-43-234345' },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122' },
   ]);
   const [newName, setNewName] = useState('');
   const [newPhone, setPhone] = useState('');
+  const [filteredPersons, setFilteredPersons] = useState(persons);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -16,7 +20,9 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    setPersons([...persons, { name: newName, phone: newPhone }]);
+    const newPersons = [...persons, { name: newName, phone: newPhone }];
+    setPersons(newPersons);
+    setFilteredPersons(newPersons);
   }
 
   function handleNumber(e) {
@@ -29,9 +35,21 @@ const App = () => {
     setNewName(valueName);
   }
 
+  function handleFilter(e) {
+    const searchValue = e.target.value;
+    if (!searchValue.length) {
+      setFilteredPersons(persons);
+      return;
+    }
+
+    setFilteredPersons(filteredPersons);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      filter show with: <input onChange={handleFilter} />
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input onChange={handleInput} value={newName} />
@@ -45,7 +63,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {filteredPersons.map((person) => (
           <li key={person.name}>
             {`${person.name}
             ${person.phone}`}
