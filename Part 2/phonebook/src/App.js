f62230a2c01059d2e63ff77895494347a34691db
Flavia Mabras <flaviamabras@hotmail.com>
2023-01-28 18:31:03 +0100

    exercise 2.8 solved

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '040-1234567' },
+  ]);
   const [newName, setNewName] = useState('');
+  const [newPhone, setPhone] = useState('');
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -10,15 +13,20 @@ const App = () => {
       (person) => person.name.toUpperCase() === newName.toUpperCase()
     );
     if (isSameName) {
-      alert(`${newName} ya está en la guía`);
+      alert(`${newName} is already added to phonebook`);
       return;
     }
-    setPersons([...persons, { name: newName }]);
+    setPersons([...persons, { name: newName, phone: newPhone }]);
+  }
+
+  function handleNumber(e) {
+    const valuePhone = e.target.value;
+    setPhone(valuePhone);
   }
 
   function handleInput(e) {
-    const value = e.target.value;
-    setNewName(value);
+    const valueName = e.target.value;
+    setNewName(valueName);
   }
 
   return (
@@ -29,13 +37,19 @@ const App = () => {
           name: <input onChange={handleInput} value={newName} />
         </div>
         <div>
+          number: <input onChange={handleNumber} value={newPhone} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>
+            {`${person.name}
+            ${person.phone}`}
+          </li>
         ))}
       </ul>
     </div>
